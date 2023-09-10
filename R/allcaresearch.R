56d7937427e86bc0177561dcafc8630a5625675f
Bo Jacobs Strom <bo.jacobsstrom@sciencespo.fr>
2023-09-10 19:26:53 +0200

    fixed care function

--- R/allcaresearch.R
+++ R/allcaresearch.R
@@ -11,8 +11,10 @@
 #'
 care <- function(x, c1 = "Description", c2 = "Title"){
   x %>%
-    dplyr::filter(stringr::str_detect(get(c1), "care") | stringr::str_detect(get(c1), "Care"))%>%
-    dplyr::filter(stringr::str_detect(get(c2), "care")| stringr::str_detect(get(c2), "Care"))
+    dplyr::filter(stringr::str_detect(get(c1), "care") |
+                    stringr::str_detect(get(c1), "Care")|
+                    stringr::str_detect(get(c2), "care")|
+                    stringr::str_detect(get(c2), "Care"))
 }
 
 
